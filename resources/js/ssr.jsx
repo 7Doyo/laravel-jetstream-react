b4a044dd5711503d2921e9bf58c9b02e835d7d36
7Doyo <asasahegn17@gmail.com>
2023-08-04 03:30:59 +0300

    commit files

---
+++ resources/js/ssr.jsx
@@ -0,0 +1,27 @@
+import ReactDOMServer from 'react-dom/server';
+import { createInertiaApp } from '@inertiajs/react';
+import createServer from '@inertiajs/react/server';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createServer((page) =>
+    createInertiaApp({
+        page,
+        render: ReactDOMServer.renderToString,
+        title: (title) => `${title} - ${appName}`,
+        resolve: (name) =>
+            resolvePageComponent(
+                `./Pages/${name}.jsx`,
+                import.meta.glob('./Pages/**/*.jsx')
+            ),
+        setup: ({ App, props }) => <App {...props} />,
+        progress: {
+            color: '#4B5563',
+            includeCSS: true,
+            showSpinner: true
+        }
+    })
+);
+
+// route('welcome', undefined, undefined, Ziggy);
